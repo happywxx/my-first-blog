383f8fa6cbcfa101e90803c859d22103712ffa09
wxx <wxxcn@outlook.com>
2017-02-19 05:16:47 +0000

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^post/$', views.post_list, name='post_list'),
     url(r'^$', views.encourage, name='encourage'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
