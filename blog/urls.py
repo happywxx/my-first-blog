2c3ffa5a0f7775390860ca27f667c5078e34ea42
wxx <wxxcn@outlook.com>
2017-02-19 02:10:46 +0000

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    #url(r'^$', views.encourage, name='encourage'),
+    url(r'^post/$', views.post_list, name='post_list'),
+    url(r'^$', views.encourage, name='encourage'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -12,5 +12,9 @@ def mourn(request):
 
 def encourage(request):
     return render(request, 'blog/index.html', {})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
 
 
